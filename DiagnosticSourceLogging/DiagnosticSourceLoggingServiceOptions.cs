053152ff8142cf453927f496634bf52461e3a0eb
itn3000 <itn3000@gmail.com>
2021-08-23 17:32:47 +0900

    add document comment for option

--- DiagnosticSourceLogging/DiagnosticSourceLoggingServiceOptions.cs
+++ DiagnosticSourceLogging/DiagnosticSourceLoggingServiceOptions.cs
@@ -5,18 +5,6 @@ using System.Collections.Generic;
 
 namespace DiagnosticSourceLogging
 {
-    public struct FormatterArg
-    {
-        public FormatterArg(string sourceName, string eventName, object arg)
-        {
-            SourceName = sourceName;
-            EventName = eventName;
-            Arg = arg;
-        }
-        public string SourceName { get; }
-        public string EventName { get; }
-        public object Arg { get; }
-    }
     public interface IDiagnosticSourceLoggingServiceOptions
     {
         /// <summary>determine whether subscribing or not for DiagnosticListener</summary>
@@ -30,6 +18,11 @@ namespace DiagnosticSourceLogging
         /// <param name="arg2">equals to DiagnosticSource.IsEnabled 3rd argument</param>
         /// <remarks>This may be called many times</remarks>
         bool IsEnabled(string sourceName, string eventName, object arg1, object arg2);
+        /// <summary>get or create callback for DiagnosticSource events</summary>
+        /// <param name="sourceName">equals to DiagnosticSource.Name</param>
+        /// <param name="eventName">equals to DiagnosticSource event name</param>
+        /// <remarks>this is called when every DiagnosticSource events, so you should cache delegate for performance.</remarks>
+        /// <returns>callback for DiagnosticSource event</returns>
         Action<ILogger, string, object> GetEventProcessor(string sourceName, string eventName);
     }
 }
